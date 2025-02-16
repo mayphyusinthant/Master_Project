4780d04832361c9c8a5fe29ccf01b330ea530808
May Phyusin Thant <mayphyusinthant_offical@outlook.com>
2025-02-16 10:07:37 +0630

    Floor C Interactive Map and Database Set Up"

--- campus_navigation/app.py
+++ campus_navigation/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'campus_database'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
+@app.route("/campus_mgmt")
+def campus_mgmt():
+    return render_template("campus_mgmt.html")
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users")
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+if __name__ == '__main__':
     app.run(debug=True)
