1cdb24fe04b13046a6fafa050f50b3d497aa4bc1
May Phyusin Thant <mayphyusinthant_offical@outlook.com>
2025-02-19 18:55:53 +0630

    updated Floor A, B and C Map and interaction

--- campus_navigation/app.py
+++ campus_navigation/app.py
@@ -13,11 +13,30 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT DISTINCT floor FROM room_info ORDER BY floor ASC")
+    floors = [row[0] for row in cur.fetchall()]
+    cur.close()
 
-@app.route("/campus_mgmt")
+    return render_template("index.html", floors=floors)
+
+@app.route("/campus_info")
 def campus_mgmt():
-    return render_template("campus_mgmt.html")
+    per_page = 25  
+    page = request.args.get('page', 1, type=int)  
+    offset = (page - 1) * per_page  
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT COUNT(*) FROM room_info")
+    total_rows = cur.fetchone()[0]
+    cur.execute("SELECT room_id, room_name, description, floor FROM room_info LIMIT %s OFFSET %s", (per_page, offset))
+    rooms = cur.fetchall()
+    
+    cur.close()
+
+    total_pages = (total_rows // per_page) + (1 if total_rows % per_page > 0 else 0)  
+    return render_template('campus_info.html', rooms=rooms, page=page, total_pages=total_pages)
+
 
 @app.route('/users')
 def users():
